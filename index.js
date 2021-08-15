f7724b14406cb58915f77aa54b84c84886edd599
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2021-08-15 16:45:21 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"];
+const event = ["birthday", "Christmas", "holiday"]
+
+function writeCards(names, event) {
+    let messages = [];
+    
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages
+}
+
+writeCards(names,"birthday");
+
+function countDown(positiveNumber) {
+    while (positiveNumber >=0) {
+        console.log(positiveNumber);
+        positiveNumber--;
+    }
+}
+
+countDown(100);
+
